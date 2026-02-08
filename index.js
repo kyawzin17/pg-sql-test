04bc9dc04a6a752d8cb46b0d3ce6d2b9bfadeba6
kyaw zin <kyawzinwin23k@gmail.com>
2026-02-08 21:18:15 +0630

    This is commit name, email & age

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({message: "This is sever!"})
+    res.json({name: "Kyaw Zin Win", email: "kyawzinwin23k@gmail.com", age: "22"})
 })
 
 app.listen(PORT, () => {
