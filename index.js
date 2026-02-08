826e668eb129c2e1b4204f7ed06610c8d5c0102b
kyaw zin <kyawzinwin23k@gmail.com>
2026-02-08 15:31:53 +0630

    This is cors server

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express= require("express");
 const app= express();
 const PORT= process.env.PORT || 5000;
 
+const cors = require("cors");
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.json({message: "This is sever!"})
 })
